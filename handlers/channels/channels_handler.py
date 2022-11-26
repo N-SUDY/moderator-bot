e2c95b86b15eda49fa6f38f2b102f9aacedfc724
hok7z <hok7zv@tutanota.com>
2022-11-26 17:50:24 +0200

    Delete todo

--- handlers/channels/channels_handler.py
+++ handlers/channels/channels_handler.py
@@ -1,8 +1,7 @@
-from load import dp,types, bot
-
+from load import dp,types
 from config import group_id
 
-# TODO: channel post forward in chat
+
 @dp.channel_post_handler()
 async def channel_handler(message:types.Message):
     await message.forward(group_id)
